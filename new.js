f436eea3bae202aefb8864328539dbaf7ea0d859
Sachin45696 <sachin45696@gmail.com>
2023-11-05 22:43:34 +0530

    First Commit

---
+++ new.js
@@ -0,0 +1,9 @@
+ const inputtag= document.getElementById('Searchinput')
+ const Iconsearch= document.getElementById('Search-Icon')
+
+
+    Iconsearch.addEventListener ('click',()=>{
+    const serchingfor=inputtag.value
+
+    console.log('serchingfor:', serchingfor);
+})
